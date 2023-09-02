cc6118ff870f7b3daf273ca9b0b3c824af3045e7
Austin Witherow <au.witherow@gmail.com>
2023-09-01 20:54:07 -0400

    switch portfolio/testimonial, add final cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import About from "@/views/about";
 import Services from "@/views/services";
 import Portfolio from "@/views/portfolio";
 import Testimonials from "@/views/testimonials";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Austin Witherow | Software Developer for Hire",
@@ -21,8 +23,18 @@ const Home = () => (
     </div>
     <div className="max-w-7xl mx-auto p-4 space-y-12">
       <Services />
-      <Portfolio />
       <Testimonials />
+      <Portfolio />
+    </div>
+    <div className="text-center max-w-3xl mx-auto my-16">
+      <h2 className="text-2xl font-bold">Is it Meant to Be?</h2>
+      <p className="my-4">
+        If you&apos;re liking what I&apos;m putting down and think that I could help you, don&apos;t hesitate. Reach out
+        today and let&apos;s get started working together.
+      </p>
+      <Button asChild>
+        <Link href="#services">Get Started</Link>
+      </Button>
     </div>
   </>
 );

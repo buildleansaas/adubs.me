7be4969c907c027439460038c1c175a5ddbbd6ff
Austin Witherow <au.witherow@gmail.com>
2023-09-14 15:48:46 -0400

    first guide data commit

---
+++ src/data/guides.ts
@@ -0,0 +1,9 @@
+export const guides = [
+  {
+    name: "Mastering pSEO with Next.js, TailwindCSS & ChatGPT!",
+    description:
+      "Unlock the secrets of programmatic SEO using Next.js, TailwindCSS, and ChatGPT. Learn how to create scalable, data-driven pages that search engines love.",
+    slug: "mastering-pseo-with-nextjs-tailwindcss-chatgpt",
+    emoji: "🚀",
+  },
+];

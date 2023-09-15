4f46843fcb7644080277f5ee9de4de4006a774c6
Austin Witherow <au.witherow@gmail.com>
2023-09-14 21:05:57 -0400

    update forgotten metadata on the guides page

--- src/app/guides/page.tsx
+++ src/app/guides/page.tsx
@@ -1,14 +1,39 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { guides } from "@/data/guides";
+import { Metadata } from "next";
 import Link from "next/link";
+import { DOMAIN } from "../sitemap";
+
+const title = "Technical Guides: Master Next.js, SEO & More!";
+const description =
+  "Explore in-depth guides by Austin Witherow. Master Next.js, pSEO, ChatGPT, and more to supercharge your web development journey.";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(`${DOMAIN}/guides`),
+  title: title,
+  description: description,
+  keywords: ["Next.js Tutorials", "pSEO", "ChatGPT Guides", "Web Development", "TailwindCSS"],
+  openGraph: {
+    title: title,
+    description: description,
+  },
+  twitter: {
+    title: title,
+    description: description,
+  },
+  authors: {
+    name: "Austin Witherow",
+    url: DOMAIN,
+  },
+};
 
 export default async function GuidesPage({}) {
   return (
     <div className="max-w-3xl mx-auto">
       <div className="my-8">
-        <h2 className="font-bold text-3xl">Guides</h2>
-        <p>Below are a collection of techincal guides to help you level up your software skills!</p>
+        <h2 className="font-bold text-3xl">{title}</h2>
+        <p>{description}</p>
       </div>
       <div className="grid grid-cols-1">
         {guides.map(({ name, description, slug }) => (

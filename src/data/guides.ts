15d77e4fa93a7296c1d3fd933b77f14f38420df3
Austin Witherow <au.witherow@gmail.com>
2023-09-15 17:04:39 -0400

    add keywords and jumbotron data

--- src/data/guides.ts
+++ src/data/guides.ts
@@ -5,5 +5,26 @@ export const guides = [
       "Unlock the secrets of programmatic SEO using Next.js, TailwindCSS, and ChatGPT. Learn how to create scalable, data-driven pages that search engines love.",
     slug: "mastering-pseo-with-nextjs-tailwindcss-chatgpt",
     emoji: "🚀",
+    keywords: [
+      "programmatic SEO tutorial",
+      "Next.js SEO guide",
+      "TailwindCSS for SEO",
+      "dynamic SEO with Next.js",
+      "automated content generation SEO",
+      "Next.js and TailwindCSS best practices",
+      "ChatGPT content creation",
+      "scalable SEO techniques",
+      "data-driven SEO with Next.js",
+    ],
+    cta_link: "",
+    jumbotron: {
+      headline: "Master the Art of pSEO!",
+      subline:
+        "Unlock scalable, data-driven strategies with Next.js, TailwindCSS, and ChatGPT that search engines adore.",
+      cta_text: "Dive In Now",
+      visual: "",
+      visual_alt:
+        "3D book cover showcasing 'Mastering pSEO with Next.js, TailwindCSS & ChatGPT' with a robot in the background.",
+    },
   },
 ];

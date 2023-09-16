01d50a7e6b092ca3f106d0229be2e61d4c0bdc2d
Austin Witherow <au.witherow@gmail.com>
2023-09-16 13:26:37 -0400

    add benefits data

--- src/data/guides.ts
+++ src/data/guides.ts
@@ -28,5 +28,74 @@ export const guides = [
       visual_alt:
         "3D book cover showcasing 'Mastering pSEO with Next.js, TailwindCSS & ChatGPT' with a robot in the background.",
     },
+    benefitsHeadline: "Unlock Potent pSEO Strategies!",
+    benefitsSubline:
+      "Delve into a unique blend of technical prowess and creative genius, turning your pSEO aspirations into tangible reality.",
+    benefits: [
+      {
+        emoji: "🎨",
+        title: "Responsive Designs with Tailwind CSS",
+        subline: "Craft stunning, SEO-optimized designs that adapt.",
+        description:
+          "Learn how to masterfully design responsive programmatic SEO pages with Tailwind CSS breakpoints. Ensuring your content looks great on any device while adhering to the best SEO practices.",
+        cta_text: "Master Responsiveness",
+      },
+      {
+        emoji: "🤖",
+        title: "Engage with ChatGPT, Your Creative Assistant",
+        subline: "Drive content without the heavy lifting.",
+        description:
+          "Harness the power of ChatGPT to generate engaging content. Let this AI assistant undertake the creative process, leaving you to steer the direction and fine-tune the results.",
+        cta_text: "Engage with AI",
+      },
+      {
+        emoji: "💻",
+        title: "Step into TypeScript & Next.js",
+        subline: "Dive deep into the realms of modern web development.",
+        description:
+          "Even if you're new to TypeScript or Next.js, this guide will navigate you through their intricacies, empowering you to develop robust, scalable, and high-performing sites.",
+        cta_text: "Start Coding",
+      },
+      {
+        emoji: "🕸️",
+        title: "Craft a Complete Web Experience",
+        subline: "It's more than just a page; it's a journey.",
+        description:
+          "Go beyond crafting individual SEO pages. Dive into advanced topics like sitemaps and the creation of multiple pages to offer a full, rich website experience to users and search engines alike.",
+        cta_text: "Expand Horizons",
+      },
+      {
+        emoji: "🎨",
+        title: "Design Graphics Effortlessly with Canva",
+        subline: "Bring your pages to life with compelling visuals.",
+        description:
+          "Graphics play a pivotal role in SEO and user engagement. Learn how to seamlessly use Canva to create captivating graphics for your pSEO pages without needing a design degree.",
+        cta_text: "Become a Designer",
+      },
+      {
+        emoji: "🔍",
+        title: "Dive into Advanced SEO Techniques",
+        subline: "Unlock hidden SEO potential in your projects.",
+        description:
+          "While technical know-how is crucial, understanding the nuances of SEO truly makes the difference. Explore strategies that aren't mainstream yet but carry immense potential for organic growth.",
+        cta_text: "Discover SEO Secrets",
+      },
+      {
+        emoji: "🌐",
+        title: "Globalize Your Content Reach",
+        subline: "Break boundaries with multilingual SEO.",
+        description:
+          "With the web being global, why shouldn't your content be? Dive into techniques for making your content accessible and optimized for multiple languages and regions, significantly broadening your reach.",
+        cta_text: "Go Global",
+      },
+      {
+        emoji: "⚙️",
+        title: "Automate & Optimize Your SEO Workflow",
+        subline: "Efficiency meets effectiveness.",
+        description:
+          "Automate repetitive tasks and processes, allowing you to focus on strategic decision-making. Learn how to utilize tools and methodologies to continuously optimize your pSEO efforts, ensuring sustained growth.",
+        cta_text: "Automate Processes",
+      },
+    ],
   },
 ];
